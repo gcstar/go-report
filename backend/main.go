96a4b1bf59dcc58c9daffc510eff959f7b4c1656
gcstar <saint198664@163.com>
2018-07-18 15:21:47 +0800

    use vue-element-admin as frontend template

--- backend/main.go
+++ backend/main.go
@@ -15,8 +15,6 @@ func main() {
 	f, _ := os.Create("log/gin.log")
 	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 	r := gin.Default()
-	r.LoadHTMLGlob("../frontend/views/**/*")
 	AddRoleController(r)
-	AddViewsController(r)
 	r.Run(":7777")
 }
--- backend/web/router/view.go
+++ backend/web/router/view.go
@@ -1,13 +1,11 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func AddViewsController(router *gin.Engine) {
-	router.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{"name": "index"})
-	})
+	// router.GET("/index", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "index.html", gin.H{"name": "index"})
+	// })
 }
