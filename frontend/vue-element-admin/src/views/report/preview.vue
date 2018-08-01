f8d5c3b8bdaa4ef518644a594f4c89debd817cfa
gcstar <saint198664@163.com>
2018-08-01 09:59:36 +0800

    modfiy report preview

--- frontend/vue-element-admin/src/views/report/preview.vue
+++ frontend/vue-element-admin/src/views/report/preview.vue
@@ -1,29 +1,26 @@
 <template>
-  <el-card>
-    <el-row>
-      <el-col v-for="item in queryColumn" :key="item.name" :span="6">
-        <el-date-picker v-if="item.formElement=='date'" value-format="yyyy-MM-dd" v-model="item.defaultValue" :placeholder="item.text"></el-date-picker>
-        <el-input v-if="item.formElement == 'string'" v-model="item.defaultValue" :placeholder="item.text"></el-input>
-      </el-col>
-      <el-col :span="4">
-        <el-button type="primary" @click="searchReportData">查 询</el-button>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-table :data="tableData">
+  <div>
+    <el-card>
+      <el-row>
+        <el-col v-for="item in queryColumn" :key="item.name" :span="6">
+          <el-date-picker v-if="item.formElement=='date'" value-format="yyyy-MM-dd" v-model="item.defaultValue" :placeholder="item.text"></el-date-picker>
+          <el-input v-if="item.formElement == 'string'" v-model="item.defaultValue" :placeholder="item.text"></el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" @click="searchReportData">查 询</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
+    <el-card>
+      <el-table :data="tableData" border stripe>
         <el-table-column v-for="item in tableColumn" :key="item.name" :prop="item.name" :label="item.text"></el-table-column>
       </el-table>
-    </el-row>
-  </el-card>
-
-  <!-- <el-card> -->
-
-  <!-- </el-card> -->
-
+    </el-card>
+  </div>
 </template>
 
 <script>
-import { findReports, getQueryColumn, getReportTableData } from '@/api/report'
+import * as report from '@/api/report'
 
 export default {
   name: 'reportPreview',
@@ -40,12 +37,12 @@ export default {
   beforeMount() {
     this.uid = this.$route.params.id
     const param = { fieldName: 'uid', keyWord: this.uid }
-    findReports(param).then(res => {
+    report.findReports(param).then(res => {
       if (res.data.data.length === 0) {
         this.$router.push('/404')
       }
       this.report = res.data.data[0]
-      getQueryColumn(this.report.uid).then(res => {
+      report.getQueryColumn(this.report.uid).then(res => {
         if (res.data.data != null) {
           this.queryColumn = res.data.data
         }
@@ -54,7 +51,7 @@ export default {
   },
   methods: {
     searchReportData() {
-      getReportTableData(this.report.uid, this.queryColumn).then(res => {
+      report.getReportTableData(this.report.uid, this.queryColumn).then(res => {
         const { data, metadata } = res.data
         this.tableColumn = metadata
         this.tableData = data
